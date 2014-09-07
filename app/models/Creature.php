0c4e77d7f2c4651b68942ab944405d1f250a954a
Andre Speelmans <andre@cosiso.nl>
2014-09-07 08:15:53 +0200

    Finished creatures without attacks, all textboxes

--- app/models/Creature.php
+++ app/models/Creature.php
@@ -4,6 +4,7 @@ class Creature extends Eloquent {
    protected $table = 'creatures';
    public $timestamps = false;
 
+   public $ranks = array('Geist' => 'Geist', 'Genius' => 'Genius', 'Archon' => 'Archon');
 
    ########################################################
    ##### Functions for updating
@@ -11,28 +12,38 @@ class Creature extends Eloquent {
    public function do_update($data) {
       $validator = Validator::make($data, array('name'         => 'required|max:64',
                                                 'quelle'       => 'required|integer|exists:quellen,id',
+                                                'rank'         => 'in:Geist,Genius,Archon',
                                                 'beschworung'  => 'required|integer',
                                                 'beherrschung' => 'required|integer',
                                                 'le'           => 'integer|min:1',
                                                 'ae'           => 'integer|min:0',
+                                                'pa'           => 'integer|min:0',
                                                 'rs'           => 'integer|min:0',
                                                 'mr'           => 'integer|min:0',
                                                 'gw'           => 'integer|min:1',
-                                                'gs'           => 'numeric|min:0'));
+                                                'gs'           => 'numeric|min:0',));
       if ($validator->passes()) {
          $is_new = ($this->exists) ? false : true;
          $this->name = ucfirst(strtolower($data['name']));
          $this->quelle_id = $data['quelle'];
+         $this->rank = @$data['rank'] ? : null;
          $this->beschworung = $data['beschworung'];
          $this->beherrschung = $data['beherrschung'];
+         $this->kampfregeln = @$data['kampfregeln'] ? : null;
+         $this->eigenschaften = @$data['eigenschaften'] ? : null;
+         $this->zauber = @$data['zauber'] ? : null;
+         $this->leihgaben = @$data['leihgaben'] ? : null;
+         $this->dienste = @$data['dienste'] ? : null;
+         $this->nachteil = @$data['nachteil'] ? : null;
          $this->description = @$data['description'] ? : null;
          $this->le = @$data['le'] ? : null;
          $this->ae = @$data['ae'] ? : null;
          $this->rs = @$data['rs'] ? : null;
-         $this->ini = @$data['ini'] ? : null;
+         $this->ini = @$data['ini'] ? str_replace('W', 'D', strtoupper($data['ini'])): null;
          $this->gs = @$data['gs'] ? : null;
          $this->mr = @$data['mr'] ? : null;
          $this->gw = @$data['gw'] ? : null;
+         $this->pa = @$data['pa'] ? : null;
          try {
             if ($this->save()) {
                return array('success' => true,
@@ -43,7 +54,7 @@ class Creature extends Eloquent {
                throw new Exception('Unknown error');
             }
          } catch (Exception $e) {
-            return array('message' => 'database-error while saving');
+            return array('message' => 'database-error while saving: ' . var_dump($this));
          }
       }
       return array('message' => 'updating');
--- app/views/magic/creature_show.blade.php
+++ app/views/magic/creature_show.blade.php
@@ -4,39 +4,53 @@
       $('#popup').hide();
    </script>
 @else
-   <h4>Edit creature</h4>
    {{ Form::open(array('name' => 'frm-creature',
                        'id'   => 'frm-creature')) }}
       {{ HTML::custom_hidden(array('name'  => 'id',
                                    'value' => $creature->id)) }}
-      {{ HTML::custom_text(array('name'  => 'name',
-                                 'label' => true,
-                                 'style' => 'width: 225px',
-                                 'value' => $creature->name,
-                                 'placeholder' => 'Type a name')) }}<br>
-      {{ HTML::custom_select(array('name'        => 'quelle',
-                                   'output'      => $quellen,
-                                   'selected'    => $creature->quelle_id,
-                                   'placeholder' => 'Select a quelle',
-                                   'style'       => 'width: 225px',
-                                   'label'       => true)) }}<br>
       <table>
          <tr>
-            <td>
+            <td colspan="4">
+               {{ HTML::custom_text(array('name'  => 'name',
+                                          'label' => true,
+                                          'style' => 'width: 225px',
+                                          'value' => $creature->name,
+                                          'placeholder' => 'Type a name')) }}
+            </td>
+         </tr>
+         <tr>
+            <td colspan="2">
+               {{ HTML::custom_select(array('name'        => 'quelle',
+                                            'output'      => $quellen,
+                                            'selected'    => $creature->quelle_id,
+                                            'placeholder' => 'Select a quelle',
+                                            'style'       => 'width: 225px',
+                                            'label'       => true)) }}
+            </td>
+            <td colspan="2">
+               {{ HTML::custom_select(array('name'   => 'rank',
+                                            'label'  => true,
+                                            'output' => $creature->ranks,
+                                            'selected' => $creature->rank,
+                                            'style'  => 'width: 225px',
+                                            'placeholder' => 'Select a rank')) }}
+            </td>
+         </tr>
+         <tr>
+            <td colspan="2">
                {{ HTML::custom_text(array('name'  => 'beschworung',
                                           'label' => 'Beschwörung',
                                           'value' => $creature->beschworung,
                                           'style' => 'width: 80px',
                                           'placeholder' => 'Enter value')) }}
             </td>
-            <td>
+            <td colspan="2">
                {{ HTML::custom_text(array('name'  => 'beherrschung',
                                           'label' => true,
                                           'value' => $creature->beschworung,
                                           'style' => 'width: 80px',
                                           'placeholder' => 'Enter value')) }}
             </td>
-            <td></td>
          </tr>
          <tr>
             <td>
@@ -60,12 +74,19 @@
                                           'style' => 'width: 80px',
                                           'placeholder' => 'Enter value')) }}
             </td>
-         </tr>
-         <tr>
             <td>
                {{ HTML::custom_text(array('name'  => 'ini',
                                           'label' => 'INI',
                                           'value' => $creature->ini,
+                                          'style' => 'width: 80px',
+                                          'placeholder' => 'Enter value')) }}
+            </td>
+         </tr>
+         <tr>
+            <td>
+               {{ HTML::custom_text(array('name'  => 'pa',
+                                          'label' => 'PA',
+                                          'value' => $creature->pa,
                                           'style' => 'width: 80px',
                                           'placeholder' => 'Enter value')) }}
             </td>
@@ -83,8 +104,6 @@
                                           'style' => 'width: 80px',
                                           'placeholder' => 'Enter value')) }}
             </td>
-         </tr>
-         <tr>
             <td>
                {{ HTML::custom_text(array('name'  => 'gw',
                                           'label' => 'GW',
@@ -92,13 +111,43 @@
                                           'style' => 'width: 80px',
                                           'placeholder' => 'Enter value')) }}
             </td>
-            <td colspan="2"></td>
          </tr>
       </table><br>
+      {{ HTML::custom_textarea(array('name'  => 'kampfregeln',
+                                     'value' => $creature->kampfregeln,
+                                     'label' => true,
+                                     'style' => 'width: 100%; height: 60px',
+                                     'placeholder' => 'Enter kampfregeln')) }}<br>
+      {{ HTML::custom_textarea(array('name'  => 'eigenschaften',
+                                     'value' => $creature->eigenschaften,
+                                     'label' => true,
+                                     'style' => 'width: 100%; height: 60px',
+                                     'placeholder' => 'Enter eigenschaften')) }}<br>
+      {{ HTML::custom_textarea(array('name'  => 'zauber',
+                                     'value' => $creature->zauber,
+                                     'label' => true,
+                                     'style' => 'width: 100%; height: 60px',
+                                     'placeholder' => 'Enter zauber')) }}<br>
+      {{ HTML::custom_textarea(array('name'  => 'leihgaben',
+                                     'value' => $creature->leihgaben,
+                                     'label' => true,
+                                     'style' => 'width: 100%; height: 60px',
+                                     'placeholder' => 'Enter leihgaben')) }}<br>
+      {{ HTML::custom_textarea(array('name'  => 'dienste',
+                                     'value' => $creature->dienste,
+                                     'label' => true,
+                                     'style' => 'width: 100%; height: 60px',
+                                     'placeholder' => 'Enter dienste')) }}<br>
+      {{ HTML::custom_textarea(array('name'  => 'nachteil',
+                                     'value' => $creature->nachteil,
+                                     'label' => true,
+                                     'style' => 'width: 100%; height: 20px',
+                                     'placeholder' => 'Enter nachteil')) }}<br>
       {{ HTML::custom_textarea(array('name'  => 'description',
                                      'value' => $creature->description,
                                      'label' => true,
-                                     'style' => 'width: 400px; height: 300px')) }}<br>
+                                     'style' => 'width: 100%; height: 80px',
+                                     'placeholder' => 'Enter description')) }}<br>
       <div class="button_bar">
          {{ HTML::custom_button(array('type' => 'submit')) }}
          {{ HTML::custom_button(array('type' => 'close-popup')) }}
